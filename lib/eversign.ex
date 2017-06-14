810d74902356830eea9bea4989dd37c5b8095018
Daniel Berkompas <himself@danielberkompas.com>
2017-06-14 19:23:03 +0100

    Initial commit

---
+++ lib/eversign.ex
@@ -0,0 +1,18 @@
+defmodule Eversign do
+  @moduledoc """
+  Documentation for Eversign.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eversign.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eversign_test.exs
@@ -0,0 +1,8 @@
+defmodule EversignTest do
+  use ExUnit.Case
+  doctest Eversign
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
