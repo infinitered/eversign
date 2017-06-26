725ace2bca97fdfa7fe0814975d01ee5ab960aef
Daniel Berkompas <himself@danielberkompas.com>
2017-06-26 22:00:31 +0100

    Read from application configuration better

--- lib/eversign.ex
+++ lib/eversign.ex
@@ -11,21 +11,23 @@ defmodule Eversign do
       config :eversign, api_module: Eversign.API.Mock
   """
 
-  @api_module Application.get_env(:eversign, :api_module) || Eversign.API.HTTP
-
   def use_template(params) do
-    @api_module.use_template(params)
+    api_module().use_template(params)
   end
 
   def list_documents(type) do
-    @api_module.list_documents(type)
+    api_module().list_documents(type)
   end
 
   def download_original(hash) do
-    @api_module.download_original(hash)
+    api_module().download_original(hash)
   end
 
   def download_final(hash) do
-    @api_module.download_final(hash)
+    api_module().download_final(hash)
+  end
+
+  defp api_module do
+    Application.get_env(:eversign, :api_module) || Eversign.API.HTTP
   end
 end
